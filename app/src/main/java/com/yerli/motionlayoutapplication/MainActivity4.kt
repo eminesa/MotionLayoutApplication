1375c80b0961a779d69da7398833c5ca5100c77d
Emine Şa <emine.sa@mgssoftware.net>
2021-10-29 19:29:10 +0300

    motion layout adding with 4 activity

---
+++ app/src/main/java/com/yerli/motionlayoutapplication/MainActivity4.kt
@@ -0,0 +1,21 @@
+package com.yerli.motionlayoutapplication
+
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.Window
+import android.view.WindowInsets
+import android.view.WindowManager
+
+class MainActivity4 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+         setContentView(R.layout.activity_main4)
+
+     }
+
+
+}
